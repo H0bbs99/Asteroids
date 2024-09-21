1d98698143d5277426f30f8b42551d00fb9e0634
H0bbs99 <jdoakes13@gmail.com>
2024-09-21 08:08:29 -0500

    Updated constants and started display loop from MAC

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ import pygame
 from constants import *
 
 def main():
-    
+    pygame.init
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
-
+    
+    #Starting Screen Display
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 if __name__ == "__main__":
     main()
