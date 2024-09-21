83673c484106f8f9206466f63c16c022488c2fcd
h0bbs99 <jdoakes13@gmail.com>
2024-09-21 15:08:32 -0500

    BD Ch 2 lesson 4 complete

--- main.py
+++ main.py
@@ -9,17 +9,28 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+    # Set FPS
+    clock = pygame.time.Clock()
+    
+    dt = 0
+
     #Starting Screen Display
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     # Game Loop
     while True:
+        # Allows the "X" button to close the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
+        # Set Screen to Black and refresh
         screen.fill("Black")
         pygame.display.flip()
+
+        #FPS Timing
+        dt = pygame.time.Clock.tick(clock, 60)/1000
+
 
 if __name__ == "__main__":
     main()
