606e437056a7c875109b21b92b130bdec9e42392
h0bbs99 <jdoakes13@gmail.com>
2024-09-28 12:41:57 -0500

    Inital update on asteroid2

--- asteroid.py
+++ asteroid.py
@@ -7,7 +7,10 @@ class Asteroid(CircleShape):
         self.radius = radius
     
 
-    def draw(self, dt):
+    def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), self.posit, self.radius, width = 2)
     
-    def update(self, screen):
+    def update(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.posit += forward * PLAYER_SPEED * dt
+        
